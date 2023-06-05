e01821b40e2bd985dad9bf5910c2e561865ac669
r1 <22638751+rewardone@users.noreply.github.com>
2023-06-04 22:25:01 -0500

    Initial refactor to generic device.

---
+++ ayla_iot_unofficial/__init__.py
@@ -0,0 +1,16 @@
+"""Python API for Ayla IoT devices"""
+
+from .ayla_iot_unofficial import (
+    AylaApi,
+    new_ayla_api
+)
+
+from .exc import (
+    AylaError,
+    AylaAuthError,
+    AylaAuthExpiringError,
+    AylaNotAuthedError,
+    AylaReadOnlyPropertyError,
+)
+
+__version__ = '1.0.0'
