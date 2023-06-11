d2fbb373dc54ab8ab3575bbb8bb8f97b1262f0da
r1 <22638751+rewardone@users.noreply.github.com>
2023-06-10 19:55:06 -0500

    build necessities. workflows.

--- setup.py
+++ setup.py
@@ -1,21 +1,20 @@
-import pathlib
-from setuptools import setup
-
-try:
-    from re2 import findall, M
-except ImportError:
-    import re
+from pathlib import Path
+from setuptools import setup, find_packages
+from re import findall, M
 
 packages = ["ayla_iot_unofficial"]
 
 # The directory containing this file
-HERE = pathlib.Path(__file__).parent
+HERE = Path(__file__).parent.resolve()
 
 # The text of the README file
-README = (HERE / "README.md").read_text()
+README = (HERE / "README.md").read_text("utf-8")
+
+# The text of the LICENSE file
+LICENSE = (HERE / "LICENSE").read_text("utf-8")
 
 # Pull the version from __init__.py so we don't need to maintain it in multiple places
-init_txt = (HERE / packages[0] / "__init__.py").read_text("utf-8")
+init_txt = (HERE / "src" / packages[0] / "__init__.py").read_text("utf-8")
 try:
     version = findall(r"^__version__ = ['\"]([^'\"]+)['\"]\r?$", init_txt, M)[0]
 except IndexError:
@@ -23,21 +22,23 @@ except IndexError:
 
 
 setup(
-    name="ayla-iot-unofficial",
+    name="ayla_iot_unofficial",
     version=version,
     description="Python API for Ayla IoT products",
     long_description=README,
     long_description_content_type="text/markdown",
     url="https://github.com/rewardone/ayla-iot-unofficial",
+    project_urls={
+        "Bug Tracker": "https://github.com/rewardone/ayla-iot-unofficial/issues"
+    },
     author="Reward One",
     author_email="rewardone@gmail.com",
-    license="MIT",
     classifiers=[
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.7",
     ],
-    packages=packages,
-    include_package_data=False,
-    install_requires=list(val.strip() for val in open("requirements.txt")),
+    package_dir={'':"src"},
+    packages=find_packages("src"),
+    include_package_data=False
 )
